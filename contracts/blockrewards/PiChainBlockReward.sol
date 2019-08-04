22348d081e74f106a069a79cef0408ca5b59c47b
Guillermo Pérez Alba <gperezalba94@gmail.com>
2019-08-04 20:22:23 +0200

    wrote cummulated instead of accumulated LOOOL

--- contracts/blockrewards/PiChainBlockReward.sol
+++ contracts/blockrewards/PiChainBlockReward.sol
@@ -41,7 +41,7 @@ contract PiChainBlockReward is BlockReward, Owned {
 
     mapping(address => bool) public onlineValidators;
     mapping(address => bool) public offlineValidators;
-    mapping(address => uint) public cummulatedCommission;
+    mapping(address => uint) public accumulatedCommission;
 
     modifier onlySystem {
         require(msg.sender == systemAddress);
@@ -116,7 +116,7 @@ contract PiChainBlockReward is BlockReward, Owned {
                     if (!offlineValidators[validNodes[i]]) {
                         uint payedPrice = manageNodes.getPayedPrice(validNodes[i]);
                         uint nodeCommission = nodesComission.mul(payedPrice).div(nodesValue);
-                        cummulatedCommission[validNodes[i]] += nodeCommission;
+                        accumulatedCommission[validNodes[i]] += nodeCommission;
                         validNodes[i].transfer(nodeCommission);
                     }
                     lastPayed = i;
